9721181eca98c71258da68bdb519ba71d2af75eb
Higor Chaves <higor_oliveira@outlook.com>
2019-09-25 19:28:04 -0400

    Header component done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Header
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "node_modules/reset-css/sass/_reset";
+
+body {
+  margin: 0;
+}
+
+html {
+  font-family: "Helvetica" !important;
+}
+.max-height {
+  height: calc(100vh - 28px);
+}
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @extend .max-height;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="header">
+    <div class="title">
+      <p class="sub">uma seleção de produtos</p>
+      <p class="principal">especial para você</p>
+      <p
+        class="phrase"
+      >Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+    </div>
+
+    <div class="buttons">
+      <button>Conheça a Linx</button>
+      <button>Ajude o algorítimo</button>
+      <button>Seus produtos</button>
+      <button>Compartilhe</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  text-align: center;
+  font-family: "Helvetica" !important;
+  max-height: 319.33px;
+  min-height: 319.33px;
+  background-color: #707070;
+  color: #ffffff;
+
+  .title {
+    .sub {
+      padding-top: 69px;
+      font-size: 20px;
+      height: 23px;
+    }
+
+    .principal {
+      font-size: 40px;
+      height: 48px;
+      font-weight: bold;
+    }
+
+    .phrase {
+      font-size: 14px;
+      height: 16px;
+      padding: 24px 0 50px 0;
+    }
+  }
+
+  .buttons {
+    padding-bottom: 61px;
+
+    button {
+      min-height: 40px;
+      max-height: 40px;
+      min-width: 160px;
+      max-width: 160px;
+      background-color: #ffffff;
+      border: 1px solid #707070;
+      border-radius: 4px;
+      color: #888888;
+      margin-right: 40px;
+      font-family: 'Helvetica';
+      font-size: 16px;
+    }
+
+    button:last-child {
+      margin-right: 0;
+    }
+  }
+}
+</style>
